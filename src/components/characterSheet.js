123a604784006e5678dbfb0fbe341dd37af648d5
Bastion Fennell <bastion.fennell@volusion.com>
2020-05-25 19:25:18 -0500

    Update stats display

--- src/components/characterSheet.js
+++ src/components/characterSheet.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 import './characterSheet.css';
 
+const StatBlock = styled.div`
+  align-items: center;
+  display: flex;
+  margin: 5px 0;
+`;
+
+const StatName = styled.div`
+  font-weight: bold;
+  width: 50px;
+`;
+
+const StatMod = styled.div`
+  align-items: center;
+  border-radius: 3px;
+  border: 1px solid black;
+  display: flex;
+  height: 50px;
+  justify-content: space-around;
+  width: 50px;
+`;
 
 const CharacterSheet = ({name, gender, occupation, equipment, stats, traits}) => {
     console.log(equipment)
     const hpLoadStats = stats.slice(-2);
-    const otherStats = stats.slice(0, stats.length -2);
-    const statElements = renderStats(otherStats);
 
     return(
         <div className="charSheet">
@@ -24,26 +43,24 @@ const CharacterSheet = ({name, gender, occupation, equipment, stats, traits}) =>
             <div className="section">
                 {renderOtherStats(hpLoadStats)}
             </div>
-            <div className="statsContainer section">
-                <span className="stats"> Stat Modifiers: </span>
-                {statElements}
+            <div>
+                {stats.slice(0, stats.length - 2).map(x => (
+                  <StatBlock key={x.shortName}>
+                    <StatName> {x.shortName} </StatName>
+                    <StatMod> {x.value}</StatMod>
+                  </StatBlock>
+                ))}
             </div>
 
         </div>
-    ) 
+    )
 }
 const renderOtherStats = (stats) => {
 
-    return stats.map(x => 
-        <span className="hpLoad">{`${x.shortName}: ${x.value}`}</span>
+    return stats.map(x =>
+        <span className="hpLoad" key={x.shortName}>{`${x.shortName}: ${x.value}`}</span>
     );
 }
-const renderStats = (stats) => {
-    return stats.map(x => 
-        <div className="stat">{`${x.shortName}: ${x.value}`}</div>
-    );
-}
-
 
 const mapStateToProps = (state) => {
     return {
