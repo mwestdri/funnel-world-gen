7656142fc143a729052d0c7892bfc4de691cd96f
Bastion Fennell <bastion.fennell@volusion.com>
2020-05-25 19:13:02 -0500

    Pull in styled-components, update button

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, {useEffect} from 'react';
+import styled from 'styled-components';
 import './App.css';
 import CharacterSheet from './components/characterSheet';
 import {connect} from 'react-redux';
 import { createCharacter } from './state/actions';
 
+const GenerateButton = styled.button`
+  border-radius: 5px;
+  border: 1px solid #073642;
+  background-color: #D90429;
+  color: #EDF2F4;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 10px;
+  margin-top: 20px;
+`;
+
 function App({ createCharacter }) {
   useEffect(() => {
     createCharacter();
-    }, []);
+    }, [createCharacter]);
   return (
     <div className="App">
         <div>And the next unfortunate villager is...</div>
         <CharacterSheet />
-        <div className="generateButton" onClick={createCharacter}>Oops, that one died...</div>
-        
+        <GenerateButton onClick={createCharacter}>Oops, that one died...</GenerateButton>
+
     </div>
   );
 }
